9cf7cd1fbae6aef5b00c593f95285d3fb8668382
AiQL.com <admin@aiql.com>
2025-05-11 00:15:36 +0800

    Add Lottie animation to chat screen

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -7,6 +7,8 @@ import router from '@/renderer/router'
 import vuetify from '@/renderer/plugins/vuetify'
 import i18n from '@/renderer/plugins/i18n'
 
+import Vue3Lottie from 'vue3-lottie'
+
 // Add API key defined in contextBridge to window object type
 declare global {
   // eslint-disable-next-line no-unused-vars
@@ -20,6 +22,6 @@ const app = createApp(App)
 const pinia = createPinia()
 pinia.use(createStatePersistence())
 
-app.use(vuetify).use(i18n).use(router).use(pinia)
+app.use(vuetify).use(i18n).use(router).use(pinia).use(Vue3Lottie)
 
 app.mount('#app')
--- src/renderer/screens/chat/ChatScreen.vue
+++ src/renderer/screens/chat/ChatScreen.vue
@@ -2,6 +2,8 @@
 import { useI18n } from 'vue-i18n'
 import ChatPage from '@/renderer/components/pages/ChatPage.vue'
 import { useMessageStore } from '@/renderer/store/message'
+import RobotJSON from '@/public/lotties/robot.json'
+
 const messageStore = useMessageStore()
 
 const { locale } = useI18n()
@@ -9,8 +11,21 @@ const { locale } = useI18n()
 
 <template>
   <ChatPage
+    v-if="messageStore.conversation.length > 0"
     :messages="messageStore.conversation"
     :language="locale"
     @request-delete="messageStore.deleteMessage"
   />
+  <v-container v-else>
+    <v-row>
+      <Vue3Lottie class="lottie-container" :animation-data="RobotJSON" />
+    </v-row>
+  </v-container>
 </template>
+
+<style scoped>
+.lottie-container {
+  height: calc(100vh - 252px);
+  width: 100vw;
+}
+</style>
